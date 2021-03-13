af4c67b840caeb708972ebe7b61601d6188055b4
ITAlexey <alexey.artemyev96.com>
2021-03-13 18:28:04 +0300

    added logic for textView elements

--- app/src/main/java/com/example/simpleapp/MainActivity.kt
+++ app/src/main/java/com/example/simpleapp/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.simpleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.simpleapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val adapter: PinCodeAdapter = PinCodeAdapter(4, 0)
+    private lateinit var binding: ActivityMainBinding
+    private var pinCode = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initRv(binding)
+        binding.number0.setOnClickListener { updatePinCode(binding.number0.text as String) }
+        binding.number1.setOnClickListener { updatePinCode(binding.number1.text as String) }
+        binding.number2.setOnClickListener { updatePinCode(binding.number2.text as String) }
+        binding.number3.setOnClickListener { updatePinCode(binding.number3.text as String) }
+        binding.number4.setOnClickListener { updatePinCode(binding.number4.text as String) }
+        binding.number5.setOnClickListener { updatePinCode(binding.number5.text as String) }
+        binding.number6.setOnClickListener { updatePinCode(binding.number6.text as String) }
+        binding.number7.setOnClickListener { updatePinCode(binding.number7.text as String) }
+        binding.number8.setOnClickListener { updatePinCode(binding.number8.text as String) }
+        binding.number9.setOnClickListener { updatePinCode(binding.number9.text as String) }
+        binding.backspaceImg.setOnClickListener {reducePinCode()}
     }
+
+    private fun updatePinCode(number: String) {
+        if (pinCode.length < adapter.itemCount) {
+            pinCode += number
+            adapter.updateState(pinCode.length)
+        }
+    }
+
+    private fun reducePinCode() {
+        if (pinCode.isNotEmpty()) {
+            pinCode = pinCode.substring(0, pinCode.length - 1)
+            adapter.updateState(pinCode.length)
+        }
+    }
+
+    private fun initRv(binding: ActivityMainBinding) {
+        val recyclerView = binding.rvPinCode
+        recyclerView.addOnItemTouchListener(RecyclerVIewDisabler())
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.setHasFixedSize(true)
+    }
+
 }
---
+++ app/src/main/java/com/example/simpleapp/PinCodeAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.simpleapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.simpleapp.databinding.ItemRvPincodeBinding
+
+class PinCodeAdapter(private val pinCodeSize: Int, private var currentPinCodeLen: Int)
+    : RecyclerView.Adapter<PinCodeAdapter.PinCodeHolder>() {
+
+    class PinCodeHolder(val binding: ItemRvPincodeBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PinCodeHolder {
+        val itemView = ItemRvPincodeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PinCodeHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: PinCodeHolder, position: Int) {
+        if (position < currentPinCodeLen) {
+            holder.binding.dotImg.setBackgroundResource(R.drawable.black_dot)
+        } else {
+            holder.binding.dotImg.setBackgroundResource(R.drawable.gray_dot)
+        }
+    }
+
+    fun updateState(pinCodeLen: Int) {
+        currentPinCodeLen = pinCodeLen
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int = pinCodeSize
+
+}
