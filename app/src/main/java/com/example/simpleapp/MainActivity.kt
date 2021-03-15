721e972412a78fe15759a2a096b983b41f9fd7c7
ITAlexey <alexey.artemyev96.com>
2021-03-15 12:54:00 +0300

    added arrow btn which appears in case of field of pin code

--- app/src/main/java/com/example/simpleapp/MainActivity.kt
+++ app/src/main/java/com/example/simpleapp/MainActivity.kt
@@ -1,50 +1,103 @@
 package com.example.simpleapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.Gravity
+import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.simpleapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val adapter: PinCodeAdapter = PinCodeAdapter(4, 0)
     private lateinit var binding: ActivityMainBinding
-    private var pinCode = ""
+    private var pinSetUpState = false
+    private var mainPin = ""
+    private var enteredPin = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initRv(binding)
-        binding.number0.setOnClickListener { updatePinCode(binding.number0.text as String) }
-        binding.number1.setOnClickListener { updatePinCode(binding.number1.text as String) }
-        binding.number2.setOnClickListener { updatePinCode(binding.number2.text as String) }
-        binding.number3.setOnClickListener { updatePinCode(binding.number3.text as String) }
-        binding.number4.setOnClickListener { updatePinCode(binding.number4.text as String) }
-        binding.number5.setOnClickListener { updatePinCode(binding.number5.text as String) }
-        binding.number6.setOnClickListener { updatePinCode(binding.number6.text as String) }
-        binding.number7.setOnClickListener { updatePinCode(binding.number7.text as String) }
-        binding.number8.setOnClickListener { updatePinCode(binding.number8.text as String) }
-        binding.number9.setOnClickListener { updatePinCode(binding.number9.text as String) }
-        binding.backspaceImg.setOnClickListener {reducePinCode()}
+        initRv()
+        trackButtons()
     }
 
-    private fun updatePinCode(number: String) {
-        if (pinCode.length < adapter.itemCount) {
-            pinCode += number
-            adapter.updateState(pinCode.length)
+    private fun trackButtons() {
+        binding.setupBtn.setOnClickListener { setUpPinCode(it) }
+        binding.number0.setOnClickListener { addNumber("0") }
+        binding.number1.setOnClickListener { addNumber("1") }
+        binding.number2.setOnClickListener { addNumber("2") }
+        binding.number3.setOnClickListener { addNumber("3") }
+        binding.number4.setOnClickListener { addNumber("4") }
+        binding.number5.setOnClickListener { addNumber("5") }
+        binding.number6.setOnClickListener { addNumber("6") }
+        binding.number7.setOnClickListener { addNumber("7") }
+        binding.number8.setOnClickListener { addNumber("8") }
+        binding.number9.setOnClickListener { addNumber("9") }
+        binding.acceptArrow.setOnClickListener { acceptPinCode() }
+        binding.backspaceImg.setOnClickListener { removeNumber() }
+    }
+
+    private fun setUpPinCode(view: View?) {
+        view?.visibility = View.GONE
+        displayViews()
+    }
+
+    private fun displayViews() {
+        binding.rvPinCode.visibility = View.VISIBLE
+        binding.infoMsg.visibility = View.VISIBLE
+        binding.numbers.visibility = View.VISIBLE
+    }
+
+    private fun acceptPinCode() {
+        if (!pinSetUpState) {
+            pinSetUpState = true
+            binding.infoMsg.text = "Input pin code"
+            binding.acceptArrow.visibility = View.INVISIBLE
+            adapter.updateState(enteredPin.length)
+        } else {
+            if (mainPin == enteredPin) {
+               Toast.makeText(applicationContext, "You are logged in!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "Incorrect pin code, try again", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
-    private fun reducePinCode() {
-        if (pinCode.isNotEmpty()) {
-            pinCode = pinCode.substring(0, pinCode.length - 1)
-            adapter.updateState(pinCode.length)
+    private fun addNumber(number: String) {
+        if (mainPin.length == adapter.itemCount - 1 || enteredPin.length == adapter.itemCount - 1)
+            binding.acceptArrow.visibility = View.VISIBLE
+        if (!pinSetUpState) {
+            if (mainPin.length < adapter.itemCount) {
+                mainPin += number
+                adapter.updateState(mainPin.length)
+            }
+        } else {
+            if (enteredPin.length < adapter.itemCount) {
+                enteredPin += number
+                adapter.updateState(enteredPin.length)
+            }
         }
     }
 
-    private fun initRv(binding: ActivityMainBinding) {
+    private fun removeNumber() {
+        binding.acceptArrow.visibility = View.INVISIBLE
+        if (!pinSetUpState) {
+            if (mainPin.isNotEmpty()) {
+                mainPin = mainPin.substring(0, mainPin.length - 1)
+                adapter.updateState(mainPin.length)
+            }
+        } else {
+            if (enteredPin.isNotEmpty()) {
+                enteredPin = enteredPin.substring(0, enteredPin.length - 1)
+                adapter.updateState(enteredPin.length)
+            }
+        }
+    }
+
+    private fun initRv() {
         val recyclerView = binding.rvPinCode
         recyclerView.addOnItemTouchListener(RecyclerVIewDisabler())
         recyclerView.adapter = adapter
